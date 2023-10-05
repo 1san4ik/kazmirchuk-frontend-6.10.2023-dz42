4ec2da99b8d61764e2fc59a70e3957b23b3133c8
Olexandr <1san4ik@gmail.com>
2023-10-06 01:40:27 +0300

    Homework 42

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <>
+    <div className='smiles'>
+      <div className='smile'><img src="./images/1.png" alt="1smile" /><div className='result'>0</div></div>
+      <div className='smile'><img src="./images/2.png" alt="2smile" /><div className='result'>0</div></div>
+      <div className='smile'><img src="./images/3.png" alt="3smile" /><div className='result'>0</div></div>
+      <div className='smile'><img src="./images/4.png" alt="4smile" /><div className='result'>0</div></div>
+      <div className='smile'><img src="./images/5.png" alt="5smile" /><div className='result'>0</div></div>
+      <div className='smile'><img src="./images/6.png" alt="6smile" /><div className='result'>0</div></div>
+      <div className='smile'><img src="./images/7.png" alt="7smile" /><div className='result'>0</div></div>
     </div>
+    <div>
+      <button>Result</button>
+      <div className="victory"></div>
+    </div>
+
+
+    </>
   );
 }
 
